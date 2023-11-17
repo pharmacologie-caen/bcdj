5c92080de42ef3c73241aa09b87fdb28a7e9f009
damienlegallois <damien.legallois@unicaen.fr>
2023-11-17 15:12:59 +0100

    created sample.R to generate a simple dataset as dat.Rdata

---
+++ sample.R
@@ -0,0 +1,6 @@
+#Create dataset to test
+
+dat <- data.frame(
+  data_cond = c("20200215", "20170303", "20220201", "20221224", "20040708", "20030526", "20210101", "20200202")
+)
+save(dat, file = "dat.Rdata")
--- tests/testthat.R
+++ tests/testthat.R
@@ -10,3 +10,25 @@ library(testthat)
 library(ptest)
 
 test_check("ptest")
+
+create_date_bin <- function(data) {
+    data$date_bin <- as.integer(data$date_cond > as.Date("2020-01-01"))
+  return(data)
+}
+
+context("Test de la création de la variable dichotomique date_bin")
+
+# Charger les données de test
+test_data <- data.frame(date_cond = as.Date(c("20190101", "2020-01-01", "2021-01-01")))
+
+test_that("La variable dichotomique date_bin est créée correctement", {
+  # Appeler la fonction
+  result_data <- create_date_bin(test_data)
+
+  # Vérifier les résultats
+  expect_equal(result_data$date_bin, c(0, 0, 1),
+               info = "La variable dichotomique date_bin n'est pas créée correctement.")
+})
+
+
+ptest::pt()
